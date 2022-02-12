bb04bdfee15a582bbf9aae244227ae88998c1a5d
Baljeet Singh <ibaljeet@outlook.com>
2022-02-12 16:48:56 +0530

    Code refactored. Added loading state to repo data.

--- app/src/main/java/com/app/composewalls/model/WallpapersItem.kt
+++ app/src/main/java/com/app/composewalls/model/WallpapersItem.kt
@@ -5,11 +5,11 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class WallpapersItem(
-	val wallpaper_name: String? = null,
-	val category_id: String? = null,
-	val wallpaper_author: String? = null,
-	val wallpaper_thumb: String? = null,
-	val wallpaper_timestamp: Long? = null,
-	val wallpaper_url: String? = null,
-	val wallpaper_provider: String? = null
-): Parcelable
+    val wallpaper_name: String? = null,
+    val category_id: String? = null,
+    val wallpaper_author: String? = null,
+    val wallpaper_thumb: String? = null,
+    val wallpaper_timestamp: Long? = null,
+    val wallpaper_url: String? = null,
+    val wallpaper_provider: String? = null
+) : Parcelable
